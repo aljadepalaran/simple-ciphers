355a6a8d0d781eff4ff3a5440cc973f743bcdf2d
Al Jade <aljade1902@gmail.com>
2020-09-06 22:23:11 +0100

    Decrypt function completed.

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -10,7 +10,9 @@ def main():
         shift = int(input("Please enter the shift: "))
         print(encrypt(plaintext, shift))
     elif user_choice == 2:
-        pass
+        ciphertext = input("Enter encryped message: ")
+        shifted = int(input("Enter shift: "))
+        print(decrypt(ciphertext, shifted))
     else:
         print("Please enter a valid choice (1/2).")
         main()  # Re-run the main function
@@ -20,7 +22,7 @@ def encrypt(e_plaintext, e_shift):  # Encrypt Function
     e_output = ""  # Blank output
     for i in e_plaintext:
         e_ascii = ord(i)  # Convert to the ascii value
-        if e_ascii < 64:
+        if e_ascii < 64:  # If character not in alphabet
             e_output = e_output + i
         else:
             e_ascii = e_ascii + e_shift % 65  # Adds the shift, rotates back to A
@@ -28,8 +30,16 @@ def encrypt(e_plaintext, e_shift):  # Encrypt Function
     return e_output
 
 
-def decrypt():  # Decrypt Function
-    pass
+def decrypt(d_plaintext, d_shift):  # Decrypt Function
+    d_output = ""
+    for i in d_plaintext:
+        d_ascii = ord(i)
+        if d_ascii < 64:
+            d_output = d_output + i
+        else:
+            d_ascii = d_ascii - d_shift % 65
+            d_output = d_output + chr(d_ascii)
+    return d_output
 
 
 main()  # Run the program
