f603d80993f704b0bb7420a7d2b7ed749c715bc4
Al Jade <aljade1902@gmail.com>
2020-09-06 02:48:16 +0100

    Completed function structures - encrypt, decrypt, main.

---
+++ caesar-cipher.py
@@ -0,0 +1,29 @@
+def main():
+    print("Welcome to CaesarCipher.")
+
+    user_choice = int(input("""
+    Please select an option (1/2)
+    1. Encrypt
+    2. Decrypt
+    """))
+
+    if user_choice == 1:
+        pass
+    elif user_choice == 2:
+        pass
+    else:
+        print("Please enter a valid choice (1/2).")
+        main()  # Re-run the main function
+
+
+def encrypt():  # Encrypt Function
+    pass
+
+
+def decrypt():  # Decrypt Function
+    pass
+
+
+main()  # Run the program
+
+
